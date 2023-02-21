2a0ed42f340420697b1600708b24d78922fbf1a7
AndyMelR <ameleror@hotmail.es>
2023-02-21 15:51:39 +0000

    fixing bugs

--- src/js/app.js
+++ src/js/app.js
@@ -22,6 +22,7 @@ import "../style/index.css";
 //         city: null
 //     }
 //  */
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -29,6 +30,10 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
   // reset the website body with the new html output
+  let twitterLink = `<li ><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>`;
+  if (variables.twitter != null)
+    twitterLink = ` <li ><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`;
+
   document.querySelector(
     "#widget_content"
   ).innerHTML = `<div class="widget">${cover}
@@ -37,10 +42,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
